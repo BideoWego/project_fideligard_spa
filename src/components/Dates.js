f36145cb5e83d83b62c6544f27dc9cadefe6c33c
Bideo Wego <bideowego@gmail.com>
2018-01-06 22:39:08 -0500

    Add min and max date display and tweak slider styles

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Dates = ({ dates, onChange, value }) => (
   <div className="Dates">
-    <h2>Date</h2>
-    <h3>{dates[value]}</h3>
+    <h2>Date: {dates[value]}</h2>
+    <div class="row justify-content-between">
+      <div class="col-4">
+        {dates[0]}
+      </div>
+      <div class="col-4 text-right">
+        {dates[dates.length - 1]}
+      </div>
+    </div>
     <input
       type="range"
       min="0"
